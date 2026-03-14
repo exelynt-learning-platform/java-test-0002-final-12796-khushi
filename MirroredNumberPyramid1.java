64f378473fba13a32a100bb863f0303bf85e9ad7
KhushiThakur0123 <thakurkhushi065@gmail.com>
2026-03-14 12:00:15 +0530

    Add files via upload

--- MirroredNumberPyramid1.java
+++ MirroredNumberPyramid1.java
@@ -1,24 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int rows = 5; // Number of rows in the pattern
+        int rows = 5;
         
         for (int i = 1; i <= rows; i++) {
-            // Print leading spaces for alignment
+            // Print leading spaces
             for (int j = 1; j <= rows - i; j++) {
-                System.out.print("  "); // Two spaces for proper alignment
+                System.out.print("  ");
             }
             
-            // Print increasing numbers (1 to i)
+            // Print increasing numbers
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             
-            // Print decreasing numbers (i-1 down to 1)
+            // Print decreasing numbers
             for (int j = i - 1; j >= 1; j--) {
                 System.out.print(j + " ");
             }
             
-            // Move to next line
             System.out.println();
         }
     }
